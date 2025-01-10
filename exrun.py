81cee877ce986e3a4d9a643e111a36bada8aa7f9
Huzaim Malik <huzaimm01@gmail.com>
2025-01-09 19:20:02 -0500

    aaa

--- exrun.py
+++ exrun.py
@@ -3,6 +3,6 @@ from canvas import Canvas
 
 if __name__ == "__main__":
     app = QApplication([])
-    canvas = Canvas(None)  # Initialize the Canvas widget
-    canvas.show()  # Display the widget
-    app.exec_()  # Start the application's event loop
+    canvas = Canvas(None)
+    canvas.show() 
+    app.exec_() 
